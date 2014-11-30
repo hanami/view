f15e9153af1c65b7f98ba5b62625c561db98dead
Benny Klotz <r3qnbenni@gmail.com>
2014-11-30 23:56:17 +0100

    Implement Scope#class, Scope#inspect, LayoutScope#class and LayoutScope#inspect [resolve #44]

--- test/view/rendering/layout_scope_test.rb
+++ test/view/rendering/layout_scope_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 describe Lotus::View::Rendering::LayoutScope do
-  describe '#respond_to?' do
-    before do
-      layout     = LayoutForScopeTest.new
-      view_scope = Lotus::View::Rendering::Scope.new(ViewForScopeTest.new)
-      @scope     = Lotus::View::Rendering::LayoutScope.new(layout, view_scope)
-    end
+  before do
+    layout     = LayoutForScopeTest.new
+    view_scope = Lotus::View::Rendering::Scope.new(ViewForScopeTest.new)
+    @scope     = Lotus::View::Rendering::LayoutScope.new(layout, view_scope)
+  end
 
+  describe '#respond_to?' do
     describe 'when the layout implements the method' do
       it 'returns true' do
         assert @scope.respond_to?(:foo), "Expected @scope to respond to `#foo'"
@@ -24,6 +24,20 @@ describe Lotus::View::Rendering::LayoutScope do
       it 'returns false' do
         assert !@scope.respond_to?(:missing), "Expected @scope to NOT respond to `#missing'"
       end
+    end
+  end
+
+  describe '#class' do
+    it 'returns proper class name' do
+      @scope.class.must_equal Lotus::View::Rendering::LayoutScope
+    end
+  end
+
+  describe '#inspect' do
+    it 'returns proper inspect String' do
+      @scope.inspect.must_include '@layout'
+      @scope.inspect.must_include '@scope'
+      @scope.inspect.must_include '%x' % (@scope.object_id << 1)
     end
   end
 end
