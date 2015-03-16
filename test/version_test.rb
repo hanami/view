bd3237d28bdc9bc15a3131dba913222f36d2b35c
Luca Guidi <me@lucaguidi.com>
2015-03-16 11:13:58 +0100

    Bump version to 0.4.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::View::VERSION do
   it 'returns current version' do
-    Lotus::View::VERSION.must_equal '0.3.0'
+    Lotus::View::VERSION.must_equal '0.4.0'
   end
 end
