086a4d3970e79ed8f506b238de0c97af3476647e
Farrel Lifson <farrel.lifson@aimred.com>
2015-06-29 00:06:52 +0200

    Modify PartialFinder to search for partial template under directory of view template, if not found search recursively from root.

--- lib/lotus/view/rendering/partial_finder.rb
+++ lib/lotus/view/rendering/partial_finder.rb
@@ -22,7 +22,36 @@ module Lotus
         #   "_sidebar.html.erb"
         PREFIX = '_'.freeze
 
+        # Find a template for a partial. Initially it will look for the
+        # partial template under the directory of the parent directory 
+        # view template, if not found it will search recursivly from 
+        # the view root.
+        #
+        # @return [Lotus::View::Template] the requested template
+        #
+        # @see Lotus::View::Rendering::TemplateFinder#find
+        def find  
+          if partial_template_exists_under_view? 
+            View::Template.new partial_template_under_view_path
+          else
+            super
+          end
+        end
+
         protected
+        def partial_template_exists_under_view?
+          File.exists?(partial_template_under_view_path)
+        end
+
+        def partial_template_under_view_path
+          Dir.glob("#{[root,view_template_dir, template_name].join(separator)}.#{format}.#{engines}").first.to_s 
+        end
+
+        def view_template_dir
+          *all, last = @view.template.split(separator)
+          all.join(separator)
+        end
+
         def template_name
           *all, last = partial_name.split(separator)
           all.push( last.prepend(prefix) ).join(separator)
