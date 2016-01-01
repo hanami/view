82fead1889265e1919f4ddf75de4b94e7b4fbcc6
Steve Hook <steve.hook@gmail.com>
2016-01-01 15:21:43 +0000

    Add a new test for PartialFinder and shared partials

--- test/partial_finder_test.rb
+++ test/partial_finder_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 describe Lotus::View::Rendering::PartialFinder do
-  it 'finds the correct partial' do
+  it 'finds the correct partial in the same directory as the parent view' do
     partial_finder = Lotus::View::Rendering::PartialFinder.new(Organisations::OrderTemplates::Action, partial: 'partial', format: 'html')
     partial_finder.find.render(format: 'html').must_match 'Order Template Partial'
 
     partial_finder = Lotus::View::Rendering::PartialFinder.new(Organisations::Action, partial: 'partial', format: 'html')
     partial_finder.find.render(format: 'html').must_match 'Organisation Partial'
+  end
+
+  it 'finds the correct partial in a different directory to the parent view' do
+    partial_finder = Lotus::View::Rendering::PartialFinder.new(Organisations::OrderTemplates::Action, partial: 'shared/sidebar', format: 'html')
+    partial_finder.find.render(format: 'html').must_match '<div id="sidebar"></div>'
   end
 end
