722958ce8d06ed07cee6863690d557bb68f3293c
Russen Guggemos <nessur@gmail.com>
2016-05-14 18:14:20 -0400

    this test shows how the template finder breaks when the Controller/Action.html.erb also exists in another subdirectory

--- test/rendering_test.rb
+++ test/rendering_test.rb
@@ -47,6 +47,15 @@ describe Hanami::View do
       rendered.must_match %(<h1>Man on the Moon!</h1>)
     end
 
+    # this test was added to show that ../templates/members/articles/index.html.erb interferres with the normal behavior
+    it 'renders the correct template when a subdirectory also exists' do
+      articles = [ OpenStruct.new(title: 'Man on the Moon!') ]
+
+      rendered = Articles::Index.render(format: :html, articles: articles)
+      rendered.wont_match %(<h1>Wrong Article Template</h1>)
+      rendered.must_match %(<h1>Man on the Moon!</h1>)
+    end
+
     describe 'calling an action method from the template' do
       it 'can call with multiple arguments' do
         RenderViewMethodWithArgs.render({format: :html}).must_include %(<h1>Hello, earth!</h1>)
