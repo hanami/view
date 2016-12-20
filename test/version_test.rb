3e2178425c2df9d91ffa791c4947996ba3117035
Luca Guidi <me@lucaguidi.com>
2016-12-20 12:52:26 +0100

    Bump version to 1.0.0.beta1

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::View::VERSION do
   it 'returns current version' do
-    Hanami::View::VERSION.must_equal '0.8.0'
+    Hanami::View::VERSION.must_equal '1.0.0.beta1'
   end
 end
