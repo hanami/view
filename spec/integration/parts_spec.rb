aafdcbc110d112f29ea5a12821a31a5003d2c533
Tim Riley <tim@openmonkey.com>
2017-03-20 21:56:17 +1100

    Show both plain and custom wrapped parts in integration spec

--- spec/integration/parts_spec.rb
+++ spec/integration/parts_spec.rb
@@ -1,32 +1,36 @@
 RSpec.describe 'parts' do
-  before do
-    module Test
-      class SpecialPart < Dry::View::Part
-        def description
-          "Custom description of #{_object}"
+  describe 'custom decorator and part classes' do
+    before do
+      module Test
+        class CustomPart < Dry::View::Part
+          def to_s
+            "Custom part wrapping #{_object}"
+          end
         end
       end
     end
-  end
 
-  it 'wraps objects in parts' do
-    decorator = Class.new(Dry::View::Decorator) do
-      def part_class(name, options)
-        name == :special ? Test::SpecialPart : super
-      end
-    end.new
+    it 'wraps objects in custom parts' do
+      decorator = Class.new(Dry::View::Decorator) do
+        def part_class(name, options)
+          name == :custom ? Test::CustomPart : super
+        end
+      end.new
 
-    vc = Class.new(Dry::View::Controller) do
-      configure do |config|
-        config.decorator = decorator
-        config.paths = SPEC_ROOT.join('fixtures/templates')
-        config.layout = nil
-        config.template = 'decorated_parts'
-      end
+      vc = Class.new(Dry::View::Controller) do
+        configure do |config|
+          config.decorator = decorator
+          config.paths = SPEC_ROOT.join('fixtures/templates')
+          config.layout = nil
+          config.template = 'decorated_parts'
+        end
 
-      expose :special
-    end.new
+        expose :custom, :ordinary
+      end.new
 
-    expect(vc.(special: 'decorated thing')).to eql '<p>Custom description of decorated thing</p>'
+      expect(vc.(custom: 'custom thing', ordinary: 'ordinary thing')).to eql(
+        '<p>Custom part wrapping custom thing</p><p>ordinary thing</p>'
+      )
+    end
   end
 end
