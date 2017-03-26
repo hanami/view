f1f8f216ff545183e1d4f25a1073cb5a881cbe45
Tim Riley <tim@openmonkey.com>
2017-03-26 23:31:21 +1100

    Add unit specs for Scope

---
+++ spec/unit/scope_spec.rb
@@ -0,0 +1,58 @@
+RSpec.describe Dry::View::Scope do
+  subject(:scope) { described_class.new(renderer: renderer, context: context, locals: locals) }
+
+  let(:locals) { {} }
+  let(:context) { double('context') }
+  let(:renderer) { double('renderer') }
+
+  describe '#render' do
+    before do
+      allow(renderer).to receive(:lookup).with('_info').and_return '_info.html.erb'
+      allow(renderer).to receive(:render)
+    end
+
+    it 'renders a partial with itself as the scope' do
+      scope.render(:info)
+      expect(renderer).to have_received(:render).with('_info.html.erb', scope)
+    end
+
+    it 'renders a partial with provided locals' do
+      scope_with_locals = described_class.new(renderer: renderer, context: context, locals: {foo: 'bar'})
+
+      scope.render(:info, foo: 'bar')
+      expect(renderer).to have_received(:render).with('_info.html.erb', scope_with_locals)
+    end
+  end
+
+  describe '#method_missing' do
+    context 'matching locals' do
+      let(:locals) { {greeting: 'hello from locals'} }
+      let(:context) { double('context', greeting: 'hello from context') }
+
+      it 'returns a matching value from the locals, in favour of a matching method on the context' do
+        expect(scope.greeting).to eq 'hello from locals'
+      end
+    end
+
+    context 'matching context' do
+      let(:context) { double('context', greeting: 'hello from context') }
+
+      it 'calls the matching method on the context' do
+        expect(scope.greeting).to eq 'hello from context'
+      end
+
+      it 'forwards all arguments to the method' do
+        blk = -> { }
+        scope.greeting 'args', &blk
+
+        expect(context).to have_received(:greeting).with('args', &blk)
+      end
+    end
+
+    describe 'no matches' do
+      it 'raises an error' do
+        expect { scope.greeting }.to raise_error(NoMethodError)
+      end
+    end
+  end
+end
