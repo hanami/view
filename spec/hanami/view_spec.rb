048c5c651a1c2eeb8fa743baaf7b985690fd4563
nerdinand <nerdinand@nerdinand.com>
2017-05-06 15:38:43 +0200

    Fix version spec

--- spec/hanami/view_spec.rb
+++ spec/hanami/view_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Hanami::View do
 
   describe Hanami::View::VERSION do
     it 'returns current version' do
-      expect(Hanami::View::VERSION).to eq '1.0.0.beta2'
+      expect(Hanami::View::VERSION).to eq '1.0.0'
     end
   end
 
