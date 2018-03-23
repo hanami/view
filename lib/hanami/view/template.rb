e736a6c8060339789f36fdf14d1b6da12197b53d
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2018-03-23 18:36:09 +0100

    Avoid Tilt warning about :disable_escape option
    Haml::TempleEngine: Option :disable_escape is invalid

--- lib/hanami/view/template.rb
+++ lib/hanami/view/template.rb
@@ -7,9 +7,17 @@ module Hanami
     # @since 0.1.0
     class Template
       def initialize(template, encoding = Encoding::UTF_8)
-        # NOTE disable_escape: true is for Slim compatibility
-        # See https://github.com/hanami/assets/issues/36
-        @_template = Tilt.new(template, nil, default_encoding: encoding, disable_escape: true)
+        default_options = {default_encoding: encoding}
+
+        options = default_options
+
+        if Tilt[template] != Tilt::HamlTemplate
+          # NOTE disable_escape: true is for Slim compatibility
+          # See https://github.com/hanami/assets/issues/36
+          options.merge!(disable_escape: true)
+        end
+
+        @_template = Tilt.new(template, nil, options)
       end
 
       # Returns the format that the template handles.
