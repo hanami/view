954a2cb9cc4385be5a6f5231081e821dfd5d1158
Luca Guidi <me@lucaguidi.com>
2018-03-23 11:26:47 +0100

    Prepare for v1.2.0.beta2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Hanami::View
 View layer for Hanami
 
+## v1.2.0.beta2 - 2018-03-23
+
 ## v1.2.0.beta1 - 2018-02-28
 
 ## v1.1.1 - 2018-02-27
--- spec/unit/hanami/view/version_spec.rb
+++ spec/unit/hanami/view/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::View::VERSION" do
   it "exposes version" do
-    expect(Hanami::View::VERSION).to eq("1.2.0.beta1")
+    expect(Hanami::View::VERSION).to eq("1.2.0.beta2")
   end
 end
