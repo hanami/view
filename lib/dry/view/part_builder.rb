1bd6fb297c23270c9abf3f53746663db0a6ea9c9
Tim Riley <tim@openmonkey.com>
2018-12-01 23:57:23 +1100

    Break out PartBuilder options manipulation for subclasses to use

--- lib/dry/view/part_builder.rb
+++ lib/dry/view/part_builder.rb
@@ -27,35 +27,53 @@ module Dry
       end
 
       def build_collection_part(name:, value:, renderer:, context:, **options)
+        collection_as = collection_options(name: name, **options)[:as]
+        item_name, item_as = collection_item_options(name: name, **options).values_at(:name, :as)
+
+        arr = value.to_ary.map { |obj|
+          build_part(name: item_name, value: obj, renderer: renderer, context: context, **options.merge(as: item_as))
+        }
+
+        build_part(name: name, value: arr, renderer: renderer, context: context, **options.merge(as: collection_as))
+      end
+
+      def collection_options(name:, **options)
         collection_as = options[:as].is_a?(Array) ? options[:as].first : nil
 
-        element_name = inflector.singularize(name).to_sym
-        element_as =
+        options.merge(as: collection_as)
+      end
+
+      def collection_item_options(name:, **options)
+        singular_name = inflector.singularize(name).to_sym
+        singular_as =
           if options[:as].is_a?(Array)
             options[:as].last if options[:as].length > 1
           else
             options[:as]
           end
 
-        arr = value.to_ary.map { |obj|
-          build_part(name: element_name, value: obj, renderer: renderer, context: context, **options.merge(as: element_as))
-        }
+        if singular_as && !singular_as.is_a?(Class)
+          singular_as = inflector.singularize(singular_as.to_s)
+        end
 
-        build_part(name: name, value: arr, renderer: renderer, context: context, **options.merge(as: collection_as))
+        options.merge(
+          name: singular_name,
+          as: singular_as,
+        )
       end
 
-      def part_class(name:, **options)
+      def part_class(name:, fallback_class: Part, **options)
         name = options[:as] || name
 
         if name.is_a?(Class)
           name
         else
-          resolve_part_class(name: name)
+          resolve_part_class(name: name, fallback_class: fallback_class)
         end
       end
 
-      def resolve_part_class(name:)
-        return Part unless namespace
+      def resolve_part_class(name:, fallback_class:)
+        return fallback_class unless namespace
 
         name = inflector.camelize(name.to_s)
 
@@ -72,7 +90,7 @@ module Dry
         if klass && klass < Part
           klass
         else
-          Part
+          fallback_class
         end
       end
     end
