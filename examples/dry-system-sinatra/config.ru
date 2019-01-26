360aceef473b3f6a91883378a7c4786aa8768884
Tim Riley <tim@openmonkey.com>
2019-01-26 15:24:13 +1100

    Add example app using dry-system and sinatra

---
+++ examples/dry-system-sinatra/config.ru
@@ -0,0 +1,3 @@
+require_relative "./system/boot"
+require "example_app/web"
+run ExampleApp::Web
---
+++ examples/dry-system-sinatra/lib/example_app/views/articles/show.rb
@@ -0,0 +1,19 @@
+require "example_app/import"
+require "example_app/view"
+
+module ExampleApp
+  module Views
+    module Articles
+      class Show < View
+        include Import["article_repo"]
+
+        config.template = "articles/show"
+
+        expose :article do |slug:|
+          article_repo.by_slug!(slug)
+        end
+      end
+    end
+  end
+end
+
