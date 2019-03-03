8a14d659bccb8cc9cece95d90f883e4bac74c9a5
Tim Riley <tim@openmonkey.com>
2019-03-03 15:17:12 +1100

    Remove redundant #to_s

--- lib/dry/view/errors.rb
+++ lib/dry/view/errors.rb
@@ -4,7 +4,7 @@ module Dry
       def initialize(template_name, lookup_paths)
         msg = [
           "Template `#{template_name}` could not be found in paths:",
-          lookup_paths.map { |path| " - #{path.to_s}"}
+          lookup_paths.map { |path| " - #{path}"}
         ].join("\n\n")
 
         super(msg)
