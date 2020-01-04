34374a5617df9622b9dc08230bb79f1c44b43c4e
Piotr Solnica <piotr.solnica@gmail.com>
2020-01-04 14:51:29 +0100

    Use shared coverage file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-if RUBY_ENGINE == 'ruby'
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "/spec/"
-  end
-end
+require_relative 'support/coverage'
 
 begin
   require 'pry-byebug'
