1e9679ceb10571170cfcbfc6340337baea2a7fc9
Sean Collins <sean@cllns.com>
2022-01-12 12:37:45 -0400

    Remove examples/ folder
    They're out of date and, while integrations are important,
    they belong in documentation and not in the codebase.

--- examples/dry-system-sinatra/system/example_app/import.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "container"
-
-module ExampleApp
-  Import = Container.injector
-end
--- examples/rails/app/views/application_view.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationView < Hanami::View
-  config.paths = Rails.root.join("app/templates")
-  config.default_context = ApplicationViewContext.new
-end
