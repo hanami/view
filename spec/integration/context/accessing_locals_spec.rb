84618f04d75904898574036781b66f7c38663ade
Tim Riley <tim@riley.id.au>
2022-05-12 17:25:54 +1000

    Provide access to locals inside context

---
+++ spec/integration/context/accessing_locals_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.describe "Context / Accessing locals" do
+  let(:view) {
+    Class.new(Hanami::View) {
+      config.paths = FIXTURES_PATH.join("integration/context/accessing_locals")
+      config.template = "accessing_locals"
+
+      expose :text, decorate: false
+    }.new
+  }
+
+  it "works" do
+    expect(view.(text: "Hello").to_s).to eq (<<~TEXT).strip
+      Locals from context: {:text=>"Hello"}<br />Locals from context in partial: {:partial_local=>"hello"}
+    TEXT
+  end
+end
