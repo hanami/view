2a9667109343ef179a2fe74a1fdbd9771bea29e8
Tim Riley <tim@riley.id.au>
2022-05-23 16:58:07 +1000

    Remove #_options and #with from Context
    Now that we have just a single context object, we don’t need to reinitialize it across renderings, so these hacks are no longer needed!

--- lib/hanami/view/context.rb
+++ lib/hanami/view/context.rb
@@ -14,64 +14,12 @@ module Hanami
     #
     # @api public
     class Context
-      include Dry::Equalizer(:_options)
       include Dry::Effects.Reader(:render_env)
       include Dry::Effects.Reader(:scope)
       include DecoratedAttributes
 
-      attr_reader :_options
-
-      # Returns a new instance of Context
-      #
-      # In subclasses, you should include an `**options` parameter and pass _all
-      # arguments_ to `super`. This allows Context to make copies of itself
-      # while preserving your dependencies.
-      #
-      # @example
-      #   class MyContext < Hanami::View::Context
-      #     # Injected dependency
-      #     attr_reader :assets
-      #
-      #     def initialize(assets:, **options)
-      #       @assets = assets
-      #       super
-      #     end
-      #   end
-      #
-      # @api public
-      def initialize(**options)
-        @_options = options
-      end
-
       def locals
         scope._locals
-      end
-
-      # Returns a copy of the Context with new options merged in.
-      #
-      # This may be useful to supply values for dependencies that are _optional_
-      # when initializing your custom Context subclass.
-      #
-      # @example
-      #   class MyContext < Hanami::View::Context
-      #     # Injected dependencies (request is optional)
-      #     attr_reader :assets, :request
-      #
-      #     def initialize(assets:, request: nil, **options)
-      #       @assets = assets
-      #       @request = reuqest
-      #       super
-      #     end
-      #   end
-      #
-      #   my_context = MyContext.new(assets: assets)
-      #   my_context_with_request = my_context.with(request: request)
-      #
-      # @api public
-      def with(**new_options)
-        self.class.new(
-          **_options.merge(new_options)
-        )
       end
     end
   end
