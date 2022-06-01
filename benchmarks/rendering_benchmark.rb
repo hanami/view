021e92ccac1ad794dc4b887c948254339f2ab5ce
Tim Riley <tim@riley.id.au>
2022-06-01 22:41:37 +1000

    Tweak benchmarks

--- benchmarks/rendering_benchmark.rb
+++ benchmarks/rendering_benchmark.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require "hotch"
 require "pathname"
 require "ostruct"
+require "benchmark/ips"
 require "hanami/view"
 
 TEMPLATES_PATHS = Pathname(__FILE__).dirname.join("templates")
@@ -11,19 +11,25 @@ TEMPLATE_LOCALS = {
   users: [
     OpenStruct.new(name: "Jane", email: "jane@example.com"),
     OpenStruct.new(name: "Teresa", email: "teresa@example.com")
-  ]
+  ] * 50
 }.freeze
 
 class View < Hanami::View
   config.paths = TEMPLATES_PATHS
   config.layout = "app"
   config.template = "users"
+  config.default_format = :html
 
   expose :users
 end
 
 view = View.new
 
-Hotch(filter: "Hanami", options: {limit: 50}) {
-  1000.times { view.(**TEMPLATE_LOCALS) }
-}
+Benchmark.ips do |x|
+  x.report(ENV['BRANCH']) do
+    100.times { view.(**TEMPLATE_LOCALS).to_s }
+  end
+
+  x.save! ENV['SAVE_FILE'] if ENV['SAVE_FILE']
+  x.compare!
+end
