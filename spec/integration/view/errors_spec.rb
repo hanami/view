7d9df9a7f4e822a87854e14b38f27889a12f754f
Tim Riley <tim@riley.id.au>
2023-01-04 07:04:49 +1100

    Move ensure_config to initialize

--- spec/integration/view/errors_spec.rb
+++ spec/integration/view/errors_spec.rb
@@ -5,18 +5,18 @@ require "hanami/view/context"
 
 RSpec.describe "View / errors" do
   specify "Raising an error when paths are not configured" do
-    view = Class.new(Hanami::View) do
+    view_class = Class.new(Hanami::View) do
       config.template = "hello"
-    end.new
+    end
 
-    expect { view.() }.to raise_error(Hanami::View::UndefinedConfigError, "no +paths+ configured")
+    expect { view_class.new }.to raise_error(Hanami::View::UndefinedConfigError, "no +paths+ configured")
   end
 
   specify "Raising an error when template is not configured" do
-    view = Class.new(Hanami::View) do
+    view_class = Class.new(Hanami::View) do
       config.paths = FIXTURES_PATH.join("integration/errors")
-    end.new
+    end
 
-    expect { view.() }.to raise_error(Hanami::View::UndefinedConfigError, "no +template+ configured")
+    expect { view_class.new }.to raise_error(Hanami::View::UndefinedConfigError, "no +template+ configured")
   end
 end
