a8500e285224ce2fa4cf92b99cc3482265811485
Tim Riley <tim@riley.id.au>
2023-03-19 21:57:16 +1100

    Include our html_safe in a prepended module

--- lib/hanami/view/html.rb
+++ lib/hanami/view/html.rb
@@ -51,8 +51,34 @@ module Hanami
           self
         end
       end
+
+      # @api private
+      # @since 2.0.0
+      module StringExtensions
+        # Returns the string as a {Hanami::View::HTML::SafeString}, ensuring the string is not
+        # automatically escaped when used in HTML view templates.
+        #
+        # @return [Hanami::View::HTML::SafeString]
+        #
+        # @api public
+        # @since 2.0.0
+        def html_safe
+          Hanami::View::HTML::SafeString.new(self)
+        end
+      end
     end
   end
+end
+
+class String
+  # Prepend our `#html_safe` method so that it takes precedence over Active Support's. When both
+  # methods are loaded, the more likely scenario is that the user will want Hanami's, since in the
+  # context of a Hanami app, Active Support is more likely to be loaded incidentally, as a
+  # transitive dependency of another gem.
+  #
+  # Having our `#html_safe` available via this module also means that a user can also choose to
+  # _undefine_ this method within the module if they'd rather use Active Support's.
+  prepend Hanami::View::HTML::StringExtensions
 end
 
 class Object
@@ -75,15 +101,4 @@ class Numeric
   end
 end
 
-class String
-  # Returns the string as a {Hanami::View::HTML::SafeString}, ensuring the string is not
-  # automatically escaped when used in HTML view templates.
-  #
-  # @return [Hanami::View::HTML::SafeString]
-  #
-  # @api public
-  # @since 2.0.0
-  def html_safe
-    Hanami::View::HTML::SafeString.new(self)
-  end
-end
+
