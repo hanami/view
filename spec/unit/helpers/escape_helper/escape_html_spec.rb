c73ca009a983c711b3e68e532f85af2ebe8eb3b2
Tim Riley <tim@riley.id.au>
2023-04-15 15:37:13 +1000

    (Temporarily) point out differences between hanami-utils escape specs and ours

--- spec/unit/helpers/escape_helper/escape_html_spec.rb
+++ spec/unit/helpers/escape_helper/escape_html_spec.rb
@@ -37,7 +37,15 @@ RSpec.describe Hanami::View::Helpers::EscapeHelper, ".escape_html" do
 
   it %(escapes "'") do
     expect(escape_html("'")).to eq "&#39;"
+
+    # result = mod.html("'".encode(encoding))
+    # expect(result).to eq "&apos;"
   end
+
+  # it "escapes '/'" do
+  #   result = mod.html("/".encode(encoding))
+  #   expect(result).to eq "&#x2F;"
+  # end
 
   it "escapes '<script>'" do
     expect(escape_html("<script>")).to eq "&lt;script&gt;"
@@ -52,9 +60,9 @@ RSpec.describe Hanami::View::Helpers::EscapeHelper, ".escape_html" do
   end
 
   it "escapes '<script>alert('xss')</script>'" do
-    # TODO:
-    #
-    # Copied this test for what was previously `#escape_html_attribute`.
+    expect(escape_html("<script>alert('xss')</script>")).to eq "&lt;script&gt;alert(&#39;xss&#39;)&lt;/script&gt;"
+
+    # This test was copied from the tests for the (now-removed) `#escape_html_attribute`.
     #
     # When using that method, the result was:
     #
@@ -64,13 +72,18 @@ RSpec.describe Hanami::View::Helpers::EscapeHelper, ".escape_html" do
     #
     # &lt;script&gt;alert(&#39;xss&#39;)&lt;/script&gt;
     #
-    # This seems just as safe? But we should double check before committing to the removal of
-    # #escape_html_attribute.
-    expect(escape_html("<script>alert('xss')</script>")).to eq "&lt;script&gt;alert(&#39;xss&#39;)&lt;/script&gt;"
+    # This seems just as safe?
   end
 
   it %(escapes '""><script>xss(5)</script>') do
     expect(escape_html('""><script>xss(5)</script>')).to eq "&quot;&quot;&gt;&lt;script&gt;xss(5)&lt;/script&gt;"
+
+    # '/' is kept verbatim above, but '&#x2F;' below from hanami-utils:
+    #
+    # result = mod.html('""><script>xss(5)</script>'.encode(encoding))
+    # expect(result).to eq "&quot;&quot;&gt;&lt;script&gt;xss(5)&lt;&#x2F;script&gt;"
+    #
+    # This difference is the same for any other tests below with '/' present.
   end
 
   it %(escapes '><script>xss(6)</script>') do
@@ -92,4 +105,19 @@ RSpec.describe Hanami::View::Helpers::EscapeHelper, ".escape_html" do
   it %(escapes '<<script>xss(14);//<</script>') do
     expect(escape_html("<<script>xss(14);//<</script>")).to eq "&lt;&lt;script&gt;xss(14);//&lt;&lt;/script&gt;"
   end
+
+  # This test from hanami-utils fails. The code here does not force conversion of strings to UTF-8.
+  # it "converts strings to UTF-8 encoding" do
+  #   skip "There is no ASCII-8BIT encoding" unless Encoding.name_list.include?("ASCII-8BIT")
+  #
+  #   # 'тест' means test in russian
+  #   string = "тест".dup.force_encoding("ASCII-8BIT")
+  #   encoding = string.encoding
+  #
+  #   result = escape_html(string)
+  #   expect(result).to eq "тест"
+  #   expect(result.encoding).to eq Encoding::UTF_8
+  #
+  #   expect(string.encoding).to eq encoding
+  # end
 end
